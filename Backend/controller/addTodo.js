feb1dd7636de2615002ae05373279ad54146e9e1
ameyayeole <yeoleameya8@gmail.com>
2023-01-11 20:07:10 +0530

    made database schema and added todo to database

--- Backend/controller/addTodo.js
+++ Backend/controller/addTodo.js
@@ -1,3 +1,14 @@
-export const addTodo = (req,res)=>{
-    console.log(req.body);
+import Todo from "../model/todo.js"
+
+export const addTodo = async (req,res)=>{
+    try{
+    const newTodo = await Todo.create({
+        data:req.body.data,
+        createdAt:Date.now()
+    })
+    await newTodo.save();
+   return res.status(200).json(newTodo);
+} catch(error){
+   return res.status(200).json(error.message);
+}
 } 
---
+++ Backend/model/todo.js
@@ -0,0 +1,19 @@
+import { text } from "express";
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    data:{
+    type:String,
+    required:true
+    },
+    done:{
+        type:Boolean,
+        default:false
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+const todo = mongoose.model('todo',todoSchema);
+export default todo;
